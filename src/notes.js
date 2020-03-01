b74edaf1cc6b617e1cf1269589fa629935d4ffbd
Paul August <paul.augustp@gmail.com>
2020-03-01 16:02:03 +0000

    added function to add note mapping through array

---
+++ src/notes.js
@@ -0,0 +1,14 @@
+const notes = [
+    {
+        id: 1,
+        title: "This is a first note title",
+        content: "This is a first note content"
+    },
+    {
+        id: 2,
+        title: "This is a second note title",
+        content: "This is a second note content"
+    },
+];
+
+export default notes;
